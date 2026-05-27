7620e5a0b7bc24956414087d278e72f062e341d7
gabriellimaferreira05-star <gabriel.lima.ferreira05@escola.pr.gov.br>
2026-05-27 11:52:27 +0000

    projeto completo

---
+++ menu.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById('mobile-menu');
+  const navList = document.getElementById('nav-list');
+
+  menuToggle.addEventListener('click', () => {
+    // Alterna a classe 'active' na lista do menu
+    navList.classList.toggle('active');
+    
+    // Opcional: Animação básica do botão (fica um 'X' se desejar customizar)
+    menuToggle.classList.toggle('is-active');
+  });
