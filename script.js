836440b91929bf8d79b7bc3aa2b5f8bde1624a28
gabriellimaferreira05-star <gabriel.lima.ferreira05@escola.pr.gov.br>
2026-05-27 11:22:33 +0000

    Página completa

---
+++ script.js
@@ -0,0 +1,14 @@
+// Mensagem ao clicar no botão
+function mostrarMensagem(){
+  alert("A energia solar ajuda o agro e protege o meio ambiente!");
+}
+
+// Modo escuro
+function modoEscuro(){
+  document.body.classList.toggle("dark-mode");
+}
+
+// Mensagem inicial
+window.onload = function(){
+  alert("Bem-vindo ao Projeto Energia Solar no Agro!");
+}
